772bdc137feed3b3ebda83a3092a40e75d1adee5
josiascastro <josiascastro2003@gmail.com>
2023-09-17 12:26:36 -0300

    update env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,17 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
+import env from '../env.js'
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDYOO5bawKlmnTII8PkKn-fp_1Zu8cUz4Y",
-    authDomain: "resurgimiento-grasa.firebaseapp.com",
-    projectId: "resurgimiento-grasa",
-    storageBucket: "resurgimiento-grasa.appspot.com",
-    messagingSenderId: "86249819183",
-    appId: "1:86249819183:web:794e09e59ac31473408ada"
+    apiKey: env.API_KEY,
+    authDomain: env.AUTH_DOMAIN,
+    projectId: env.PROJECT_ID,
+    storageBucket: env.STORAGE_BUCKET,
+    messagingSenderId: env.MESSAGING_SENDER_ID,
+    appId: env.APP_ID,
 }
+
 firebase.initializeApp(firebaseConfig)
 
 export const fb = firebase
